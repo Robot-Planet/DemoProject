baa43ab2d26b3384fb95b1714c63237f56499ff6
rBotPlanet <rbotplanet@gmail.com>
2025-02-08 16:06:34 +0700

    Commit develop test mail noti

--- tests/demoproject.spec.ts
+++ tests/demoproject.spec.ts
@@ -1,7 +1,8 @@
 const {test, expect} = require('@playwright/test')
 
 test ('validate page title' , async ({ page}) => {
-    await page.goto('https://www.saucedemo.com/');
+    //await page.goto('https://www.saucedemo.com/');
+    await page.goto('https://www.xxx.saucedemo.com/');
     await expect(page).toHaveTitle('Swag Labs')
 
 });
